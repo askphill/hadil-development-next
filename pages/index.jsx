c5d87de261d3420c268f1e65c0d2df822b8d2fbf
Hadil Alabrash <35224912+hadeel43@users.noreply.github.com>
2025-04-10 10:12:47 +0200

    feat(app): Display the Resource

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,11 @@
 import SEO from 'components/SEO';
-import { getPageType, getPage } from 'lib/contentful';
+import { getPageType, getPage, getGlobal } from 'lib/contentful';
 import PageGeneral from 'components/pages/general';
 import Layout from 'components/layout';
 import PageFiveHundred from 'components/pages/fiveHundred';
 
 // eslint-disable-next-line arrow-body-style
-const Home = ({ page, error, previewData }) => {
+const Home = ({ page, error, previewData, global }) => {
   // if (error) {
   //   return <PageFiveHundred data={page} />;
   // }
@@ -22,7 +22,7 @@ const Home = ({ page, error, previewData }) => {
           backgroundColor: 'red',
         }}
       >
-        Hello World
+        {global.layout.testResource?.text}
       </div>
       {/* {page?.seo && <SEO seo={page.seo} />} */}
       {/* <PageGeneral data={page} /> */}
@@ -35,7 +35,8 @@ export async function getStaticProps({ locale, preview = false, previewData = nu
   const type = await getPageType('/', locale);
 
   const page = await getPage(type, '/', locale, preview);
-
+  const global = await getGlobal(['layout'], locale, preview);
+  console.log('global', global);
   if (page.errors) {
     return {
       props: { page, error: true, previewData },
@@ -43,7 +44,7 @@ export async function getStaticProps({ locale, preview = false, previewData = nu
   }
 
   return {
-    props: { page, previewData },
+    props: { page, previewData, global },
     revalidate: 1,
   };
 }
