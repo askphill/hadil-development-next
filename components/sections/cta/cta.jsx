eb8d726ed7dc3bd431f36b581cb3d8d12d9ddd98
Hadil Alabrash <35224912+hadeel43@users.noreply.github.com>
2025-04-14 17:06:13 +0200

    fix (cta section): adjust styling

--- components/sections/cta/cta.jsx
+++ components/sections/cta/cta.jsx
@@ -5,7 +5,7 @@ const Cta = ({ data }) => {
   const link = button?.link?.externalLink || '';
 
   return (
-    <section className="px-7 bg-black text-white h-[80vh] flex items-center w-full lg:px-44 relative">
+    <section className="px-7 bg-black text-white min-h-[80vh] flex lg:items-center w-full py-48 lg:px-44 relative">
       <div className="flex flex-col gap-y-44 lg:gap-y-16 w-full">
         {image && (
           <div className="absolute -top-12 hidden lg:block right-32">
@@ -18,16 +18,16 @@ const Cta = ({ data }) => {
             />
           </div>
         )}
-        <div className="flex flex-col gap-y-7 lg:gap-y-16 w-full">
+        <div className="flex flex-col gap-y-7 lg:gap-y-16 w-full  flex-1 justify-end">
           {subtitle && <p className="text-2xl lg:text-5xl uppercase">{subtitle}</p>}
-          <div className="[&>p]:font-bold [&>p]:text-7xl [&>p]:lg:text-[10rem] ">
+          <div className="[&>p]:font-bold [&>p]:text-7xl [&>p]:lg:text-[9rem] ">
             <RichText text={title.json} />
           </div>
           {description && <p className="text-2xl lg:text-3xl uppercase">{description}</p>}
         </div>
 
         {button && link && (
-          <div className="rounded-full p-[2px] w-full lg:w-fit bg-[linear-gradient(to_right,_#622888,_#BC5548,_#EBC286,_#F6F0DA)] inline-block mx-2 lg:mx-0">
+          <div className="rounded-full mt-auto p-[2px] w-full lg:w-fit bg-[linear-gradient(to_right,_#622888,_#BC5548,_#EBC286,_#F6F0DA)] inline-block mx-2 lg:mx-0">
             <a
               type="button"
               alt={button.buttonText}
