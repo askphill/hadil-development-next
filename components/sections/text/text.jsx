f8193804220fc3811dd96698934b7ac93d8d749b
Hadil Alabrash <35224912+hadeel43@users.noreply.github.com>
2025-04-16 14:08:38 +0200

    Update text.jsx

--- components/sections/text/text.jsx
+++ components/sections/text/text.jsx
@@ -5,10 +5,12 @@ const Text = ({ data }) => {
   return (
     <section className="px-8 bg-black text-white flex items-center w-full lg:px-0 lg:py-24 py-12 relative">
       <div className="flex flex-col gap-y-9 lg:flex-row lg:gap-x-32 justify-between w-full lg:max-w-6xl mx-auto">
-        {textTitle && <h2 className="font-bold text-4xl lg:text-7xl">{textTitle}</h2>}
-        <div className="lg:max-w-xl [&>p]:text-xl [&>p]:md:text-2xl ">
-          <RichText text={textContent.json} />
-        </div>
+        {textTitle && <p className="font-bold text-4xl lg:text-7xl">{textTitle}</p>}
+        {textContent.json && (
+          <div className="lg:max-w-xl [&>p]:text-xl [&>p]:md:text-2xl ">
+            <RichText text={textContent.json} />
+          </div>
+        )}
       </div>
     </section>
   );
