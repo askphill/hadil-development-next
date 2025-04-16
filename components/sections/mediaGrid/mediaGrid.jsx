2780f95cbd4681500ee8518068a56e5383881b5b
Hadil Alabrash <35224912+hadeel43@users.noreply.github.com>
2025-04-16 14:43:00 +0200

    Resolve PR issues

--- components/sections/mediaGrid/mediaGrid.jsx
+++ components/sections/mediaGrid/mediaGrid.jsx
@@ -1,23 +1,20 @@
 import { Image } from 'components/shared';
 
 const MediaGrid = ({ data }) => {
-  const { media } = data;
-
+  const { media, name } = data;
+  if (!media || !media.length) return null;
   return (
     <section
       className="px-8 bg-black flex items-center w-full lg:px-0 lg:py-24 py-12 relative"
       aria-labelledby="media-grid-heading"
     >
       <h2 id="media-grid-heading" className="sr-only">
-        Media Gallery
+        {name || 'Media Grid'}
       </h2>
-      <div className="w-full max-w-7xl mx-auto grid grid-cols-12 gap-6 g:gap-14">
+      <div className="w-full max-w-7xl mx-auto grid grid-cols-12 gap-6 lg:gap-14">
         {media.map((item, index) => {
-          const mod = index % 3;
-          let colSpan = '';
-          if (mod === 0) colSpan = 'col-span-5';
-          if (mod === 1) colSpan = 'col-span-7';
-          if (mod === 2) colSpan = 'col-span-12';
+          const spanList = ['col-span-5', 'col-span-7', 'col-span-12'];
+          const colSpan = spanList[index % 3];
 
           return (
             <div key={item.title || index} className={`${colSpan} flex justify-center`}>
