d4117d81ffc32dc18780ea40fd956ae2e58aa412
Hadil Alabrash <35224912+hadeel43@users.noreply.github.com>
2025-04-16 11:26:57 +0200

    Clean up

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -27,7 +27,6 @@ export default async function handler(req, res) {
 
     return res.status(200).json({ message: 'Email sent successfully' });
   } catch (error) {
-    console.error('Email sending error:', error);
     return res.status(500).json({ message: 'Failed to send email' });
   }
 }
