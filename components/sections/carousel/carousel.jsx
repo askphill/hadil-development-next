fea2099d1555e33430ae30dc27ef9fd2d8ed1fb3
Hadil Alabrash <35224912+hadeel43@users.noreply.github.com>
2025-04-16 14:29:55 +0200

    Reolve logos section issues

--- components/sections/carousel/carousel.jsx
+++ components/sections/carousel/carousel.jsx
@@ -1,19 +1,26 @@
+/* eslint-disable react/no-array-index-key */
 /* eslint-disable import/no-extraneous-dependencies */
 import Marquee from 'react-fast-marquee';
 import { Image } from 'components/shared';
 
 const Carousel = ({ data }) => {
   const { logos } = data;
-
+  if (!logos.length) return null;
   return (
     <section className="w-full bg-black lg:py-24 py-12" aria-labelledby="carousel-heading">
       <h2 id="carousel-heading" className="sr-only">
         Partner Logos Carousel
       </h2>
       <Marquee speed={60} gradient={false} pauseOnHover={false} direction="left">
-        {logos.map(logo => (
-          <div key={logo.title} className="py-8 px-10  lg:mx-7 lg:p-10 bg-black">
-            <Image src={logo} className="w-40 lg:w-60 h-auto" layout="" loading="lazy" />
+        {logos.map((logo, index) => (
+          <div key={`${logo.title}-${index}`} className="py-8 px-10  lg:mx-7 lg:p-10 bg-black">
+            <Image
+              src={logo}
+              alt={logo.title || 'Logo'}
+              className="w-40 lg:w-60 h-auto"
+              layout=""
+              loading="lazy"
+            />
           </div>
         ))}
       </Marquee>
