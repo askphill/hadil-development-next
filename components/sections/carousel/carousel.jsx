eb644eecb771109932ee8cfcf90684d55be779aa
Hadil Alabrash <35224912+hadeel43@users.noreply.github.com>
2025-04-16 13:27:14 +0200

    Code improvements

--- components/sections/carousel/carousel.jsx
+++ components/sections/carousel/carousel.jsx
@@ -6,11 +6,14 @@ const Carousel = ({ data }) => {
   const { logos } = data;
 
   return (
-    <section className="w-full bg-black lg:py-24 py-12">
+    <section className="w-full bg-black lg:py-24 py-12" aria-labelledby="carousel-heading">
+      <h2 id="carousel-heading" className="sr-only">
+        Partner Logos Carousel
+      </h2>
       <Marquee speed={60} gradient={false} pauseOnHover={false} direction="left">
         {logos.map(logo => (
           <div key={logo.title} className="py-8 px-10  lg:mx-7 lg:p-10 bg-black">
-            <Image src={logo} className="w-40 lg:w-60 h-auto " layout="" loading="eager" priority />
+            <Image src={logo} className="w-40 lg:w-60 h-auto" layout="" loading="lazy" />
           </div>
         ))}
       </Marquee>
